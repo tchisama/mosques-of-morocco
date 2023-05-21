6a8d4776198500bfbde8d157695dd30a328d1c68
tchi-sama <tchisama41@gmail.com>
2023-05-21 19:53:54 +0100

    working with expo maps

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView ,{Marker} from 'react-native-maps';
 
 export default function App() {
+  const [mapRegion,setMapRegion]= useState({
+    latitude:37.78825,
+    longitude:-122.4324,
+    latitudeDelta:0.0922,
+    longitudeDelta:0.0421,
+  })
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <MapView region={mapRegion} style={styles.map} >
+        <Marker
+        coordinate={mapRegion}
+        title="masjid mohammed 6"
+        description="its the big masjid in morocco so yah!"
+        />
+      </MapView>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +31,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  map:{
+    width:"100%",
+    height:"100%"
+  }
 });
