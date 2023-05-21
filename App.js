9a124f6d54e7227f7896c81b7b3ede1a2c9b5716
tchi-sama <tchisama41@gmail.com>
2023-05-21 20:57:20 +0100

    setup map for expo

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
+import { useEffect, useLayoutEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import MapView ,{Marker} from 'react-native-maps';
+import * as Location from "expo-location";
+
 
 export default function App() {
   const [mapRegion,setMapRegion]= useState({
@@ -10,14 +12,36 @@ export default function App() {
     latitudeDelta:0.0922,
     longitudeDelta:0.0421,
   })
+
+
+  const userLocation = async ()=>{
+    let {status } = await Location.requestForegroundPermissionsAsync();
+    if(status!== "granted"){
+      setErrorMsg("Permission to access location was denied")
+    }
+    let location = await Location.getCurrentPositionAsync({
+      enableHighAccuracy : true
+    })
+    setMapRegion({
+    latitude:location.coords.latitude,
+    longitude:location.coords.longitude,
+    latitudeDelta:0.0922,
+    longitudeDelta:0.0421,
+    })
+  }
+
+  useEffect(()=>{
+    userLocation()
+  },[])
   return (
     <View style={styles.container}>
       <MapView region={mapRegion} style={styles.map} >
-        <Marker
+        <MapView.Marker
         coordinate={mapRegion}
         title="masjid mohammed 6"
         description="its the big masjid in morocco so yah!"
         />
+        <View style={styles.marker}></View>
       </MapView>
       <StatusBar style="auto" />
     </View>
@@ -34,5 +58,10 @@ const styles = StyleSheet.create({
   map:{
     width:"100%",
     height:"100%"
+  },
+  marker:{
+    width:"20px",
+    height:"20px",
+    backgroundColor:"red",
   }
 });
