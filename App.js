b9e0df2846b92981c456824f53d0b85f637630f1
tchi-sama <tchisama41@gmail.com>
2023-05-30 14:01:04 +0100

    last update

--- App.js
+++ App.js
@@ -5,10 +5,13 @@ import MapView , {Callout, Marker}from 'react-native-maps';
 import * as Location from "expo-location";
 import  Icon  from "@expo/vector-icons/Ionicons"
 import WebView from 'react-native-webview';
+import MapViewDirections from "react-native-maps-directions"
 
-const mapTheme = [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"color":"#ffffff"},{"visibility":"on"},{"saturation":0}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"color":"#596549"},{"visibility":"on"},{"saturation":0}]},{"featureType":"road","elementType":"geometry","stylers":[{"color":"#bdbf5f"},{"visibility":"on"},{"saturation":0}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#818a60"},{"visibility":"on"},{"saturation":0}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#606c38"},{"visibility":"on"},{"saturation":0}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#596549"},{"visibility":"on"},{"saturation":0}]},{"featureType":"water","elementType":"","stylers":[{"color":"#a2b389"},{"visibility":"on"},{"saturation":0}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"on"}]},{"featureType":"all","elementType":"labels.text","stylers":[{"visibility":"on"}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"}]}]
+import { SpecialScrollView, SpecialView } from 'react-native-scroll-to-element';
 
 
+
+const mapTheme = [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"color":"#606c30"},{"visibility":"on"},{"saturation":0}]},{"featureType":"road","elementType":"geometry","stylers":[{"color":"#c4d09a"},{"visibility":"on"},{"saturation":0}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#fefae0"},{"visibility":"on"},{"saturation":0}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#eeebd2"},{"visibility":"on"},{"saturation":0}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#eeebd2"},{"visibility":"on"},{"saturation":0}]},{"featureType":"water","elementType":"","stylers":[{"color":"#9099a2"},{"visibility":"on"},{"saturation":0}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"off"}]}]
 
 const mosques = [
   {
@@ -19,7 +22,12 @@ const mosques = [
       longitudeDelta:0.0421,
     },
     name:"Koutoubia",
-    img:"https://lh5.googleusercontent.com/p/AF1QipON_zhK7b4-bNGFN7rk1LLqsHxu025LWgidoTIN=w408-h244-k-no"
+    names:["koutoubia",'كتبية'],
+    img:"https://lh5.googleusercontent.com/p/AF1QipON_zhK7b4-bNGFN7rk1LLqsHxu025LWgidoTIN=w408-h244-k-no",
+    women:true,
+    friday:true,
+    id:0,
+    bathrooms:false,
   },
   {
     location:{
@@ -29,7 +37,11 @@ const mosques = [
       longitudeDelta:0.0421,
     },
     name:"Ben Youssef Mosque",
-    img:"https://lh5.googleusercontent.com/p/AF1QipMb84JafmzeAQwsSsBt6Xp_9k1DvM3WsCwBa_Mj=w408-h725-k-no"
+    img:"https://lh5.googleusercontent.com/p/AF1QipMb84JafmzeAQwsSsBt6Xp_9k1DvM3WsCwBa_Mj=w408-h725-k-no",
+    women:false,
+    id:1,
+    friday:true,
+    bathrooms:true,
   },
   {
     location:{
@@ -39,7 +51,39 @@ const mosques = [
       longitudeDelta:0.0421,
     },
     name:"Moulay el Yazid Mosque",
-    img:"https://lh5.googleusercontent.com/p/AF1QipONLhG9qQEOrGFza0NNuKtnqKE0rt0QIM5vs_Hp=w408-h306-k-no"
+    img:"https://lh5.googleusercontent.com/p/AF1QipONLhG9qQEOrGFza0NNuKtnqKE0rt0QIM5vs_Hp=w408-h306-k-no",
+    women:true,
+    id:2,
+    friday:false,
+    bathrooms:false,
+  },
+  {
+    location:{
+      latitude:31.555249128598337, 
+      longitude: -7.6871191294477885, 
+      latitudeDelta:0.0922,
+      longitudeDelta:0.0421,
+    },
+    name:"مسجد حي ايت منصور",
+    img:"https://maps.gstatic.com/tactile/pane/default_geocode-2x.png",
+    women:false,
+    id:3,
+    friday:false,
+    bathrooms:false,
+  },
+  {
+    location:{
+      latitude:31.56141570680596, 
+      longitude:  -7.670286834211737, 
+      latitudeDelta:0.0922,
+      longitudeDelta:0.0421,
+    },
+    name:"مسجد الغفران",
+    img:"https://lh5.googleusercontent.com/p/AF1QipOlMKGs3uH89hB9ivhV_TADjEO88B1gEh6C6kq9=w408-h544-k-no",
+    women:false,
+    id:4,
+    friday:false,
+    bathrooms:false,
   },
 ]
 
@@ -53,18 +97,28 @@ const mosques = [
   const [mapRegion,setMapRegion]= useState({
     latitude:37.78825,
     longitude:-122.4324,
-    latitudeDelta:0.0922,
-    longitudeDelta:0.0421,
+    latitudeDelta:2,
+    longitudeDelta:2,
   })
+
+
+
+  const [friday,setFriday]=useState(false);
+  const [women,setWomen]=useState(false);
+  const [bathrooms,setBathrooms]=useState(false);
+
+  const [selectedMosque,setSelectedMosque]=useState(null);
+
 
 
   const [openSearch,setOpenSearch]=useState(false)
   const [search,setSearch]=useState("")
   const _map = useRef(null);
-
+  const [filteredMosquesState,SetFilteredMosquesState]=useState(mosques)
 
   const userLocation = async ()=>{
     let {status } = await Location.requestForegroundPermissionsAsync();
+
     if(status!== "granted"){
       setErrorMsg("Permission to access location was denied")
     }
@@ -74,8 +128,8 @@ const mosques = [
     setMapRegion({
     latitude:location.coords.latitude,
     longitude:location.coords.longitude,
-    latitudeDelta:0.025,
-    longitudeDelta:0.025,
+    latitudeDelta:0.045,
+    longitudeDelta:0.045,
     })
   }
 
@@ -86,86 +140,124 @@ const mosques = [
     _map.current.animateToRegion(
       {
         ...coordinate,
-        latitudeDelta:0.005,
-        longitudeDelta:0.005,
+        latitudeDelta:0.018,
+        longitudeDelta:0.018,
       }
     )
   }
 
-
+  const gohome=()=>{
+    GoTo(mapRegion)
+  }
 
 
   useEffect(()=>{
     userLocation()
   },[])
+
+
   useEffect(()=>{
-    console.log(search)
-  },[search])
+    let final = mosques.filter(m=>m.name.toLocaleLowerCase().includes(search.toLocaleLowerCase()))
+    if (friday) {
+      final= final.filter(m=>m.friday)
+    }
+    if (women) {
+      final=final.filter(m=>m.women)
+    }
+    if (bathrooms) {
+      final=final.filter(m=>m.bathrooms)
+    }
+    SetFilteredMosquesState(final)
+    console.log("hi")
+    if(final&&search){
+      setSelectedMosque(final[0].id)
+      GoTo(final[0].location)
+    }
+  },[search,bathrooms,women,friday])
   return (
     <View  className="flex-1 bg-blue-200">
       <View className="absolute top-10  p-4 z-10 w-full flex-row justify-between">
 
         {
           !openSearch &&
-        <TouchableOpacity  onPress={()=>setOpenSearch(p=>!p)} className=" t.shadow2xl  w-10 h-10 mr-2 justify-center items-center rounded-full bg-[#e9f1ce] border border-white">
-          <Icon name={"search"} size={25} color={"#606C38"}></Icon>
+        <TouchableOpacity  onPress={()=>setOpenSearch(p=>!p)} className=" t.shadow2xl  w-10 h-10 mr-2 justify-center items-center rounded-full bg-[#606C38] ">
+          <Icon name={"search"} size={22} color={"#fff"}></Icon>
         </TouchableOpacity>
         }
 
         { openSearch &&
-        <View className="flex-row flex-1 bg-[#e9f1ce] border t.shadow2xl border-white items-center px-2 rounded-full ">
-          <TouchableOpacity onPress={()=>{setOpenSearch(p=>!p);setSearch("")}} className=" justify-center mr-2 items-center rounded-full ">
-            <Icon name={"arrow-back"} size={25} color={"#606C38"}></Icon>
+        <View style={{ elevation:4 }} className="flex-row flex-1 bg-white  t.shadow3xl  items-center  rounded-full ">
+          <TouchableOpacity onPress={()=>{setOpenSearch(p=>!p);setSearch("")}} className=" w-10 h-10 bg-[#606C38] justify-center mr-2 items-center rounded-full ">
+            <Icon name={"arrow-back"} size={22} color={"#fff"}></Icon>
           </TouchableOpacity>
-          <TextInput value={search} onChangeText={setSearch} placeholder='search a mosque' className="flex-1 h-full  pr-4  rounded-full "></TextInput>
+          <TextInput value={search} onChangeText={setSearch} placeholder='ابحث عن مسجد' className="text-black  flex-1 h-full  pr-4  rounded-full "></TextInput>
         </View>
         }
-        <TouchableOpacity className=" w-10 h-10 ml-2 justify-center items-center t.shadow2xl  rounded-full bg-[#e9f1ce] border border-white">
-          <Icon name="menu" size={25} color={"#606C38"}></Icon>
+        <TouchableOpacity className=" w-10 h-10 ml-2 justify-center items-center t.shadow2xl  rounded-full bg-[#606C38] ">
+          <Icon name="menu" size={22} color={"#fff"}></Icon>
         </TouchableOpacity>
       </View>
         {
           openSearch&&
 
-      <ScrollView horizontal className="absolute left-0 top-24   w-full py-2 flex-row  z-10">
+      <ScrollView horizontal  className="absolute left-0 top-24   w-full py-2 flex-row  z-10">
         <View className="w-3"></View>
-        <TouchableOpacity className=" mx-1 p-2 px-4 w-fit justify-center items-center rounded-full bg-[#545c3999] ">
-            <Text className="text-white">hello world</Text>
+        <TouchableOpacity onPress={()=>setFriday(p=>!p)} className=" mx-1 p-2 w-fit justify-center flex-row items-center rounded-full bg-[#545c39ee] ">
+            <Text className="text-white px-2">صلاة الجمعة</Text>
+            <Icon name={friday?"checkmark-circle-sharp":"ellipse-outline"} size={20} color={"#fff"}></Icon>
         </TouchableOpacity>
-        <TouchableOpacity className=" mx-1 p-2 px-4 w-fit justify-center items-center rounded-full bg-[#545c3999] ">
-            <Text className="text-white">hello world</Text>
+        <TouchableOpacity onPress={()=>setWomen(p=>!p)} className=" mx-1 p-2 w-fit justify-center flex-row items-center rounded-full bg-[#545c39ee] ">
+            <Text className="text-white px-2">مسجد نساء</Text>
+            <Icon name={women?"checkmark-circle-sharp":"ellipse-outline"} size={20} color={"#fff"}></Icon>
         </TouchableOpacity>
-        <TouchableOpacity className=" mx-1 p-2 px-4 w-fit justify-center items-center rounded-full bg-[#545c3999] ">
-            <Text className="text-white">hello world</Text>
+        <TouchableOpacity onPress={()=>setBathrooms(p=>!p)} className=" mx-1 p-2 w-fit justify-center flex-row items-center rounded-full bg-[#545c39ee] ">
+            <Text className="text-white px-2">دورات المياه</Text>
+            <Icon name={bathrooms?"checkmark-circle-sharp":"ellipse-outline"} size={20} color={"#fff"}></Icon>
         </TouchableOpacity>
-        <TouchableOpacity className=" mx-1 p-2 px-4 w-fit justify-center items-center rounded-full bg-[#545c3999] ">
-            <Text className="text-white">hello world</Text>
-        </TouchableOpacity>
-        <View className="w-3"></View>
       </ScrollView>
 
         }
 
-
-
-      <MapView ref={_map} customMapStyle={mapTheme} region={mapRegion}  className="flex-1 w-full" >
-        <Marker
-          coordinate={mapRegion}
-          title="Your location"
-        >
-          <Image className="w-[35px] h-[42px]" source={require("./assets/UserMarker.png")}/>
-        </Marker>
-        
+        <TouchableOpacity  onPress={()=>gohome()} style={{bottom:(openSearch?150:100)}} className={` absolute z-10  right-2 t.shadow2xl  w-10 h-10 mr-2 justify-center items-center rounded-full bg-[#606c38]  border border-white`}>
+          <Icon name={"locate-outline"} size={25} color={"#e9f1ce"}></Icon>
+        </TouchableOpacity>
 
         {
-          mosques.filter(m=>m.name.toLocaleLowerCase().includes(search.toLocaleLowerCase())).map((m,key)=>{
+          selectedMosque!=null &&
+          <TouchableOpacity   style={{bottom:(openSearch?194:145)}} className={` absolute z-10  right-2 t.shadow2xl  w-10 h-10 mr-2 justify-center items-center rounded-full bg-[#606c38]  border border-white`}>
+            <Icon name={"navigate"} size={22} color={"#e9f1ce"}></Icon>
+          </TouchableOpacity>
+        }
+
+      
+      <MapView onPress={()=>setSelectedMosque(null)} ref={_map} customMapStyle={mapTheme} region={mapRegion}  className="flex-1 w-full" >
+        
+
+        {/* <MapViewDirections
+            origin={{
+              latitude:37.78825,
+              longitude:-122.4324,
+              latitudeDelta:0.0922,
+              longitudeDelta:0.0421,
+            }}
+            destination={mapRegion}
+            apikey={}
+        ></MapViewDirections> */}
+
+        {
+          filteredMosquesState.map((m,key)=>{
             return (
               <Marker
                 key={key}
                 coordinate={m.location}
                 title={m.name}
+                onPress={()=>setSelectedMosque(m.id)}
               >
-                <Image className="w-[35px] h-[42px]" source={require("./assets/marker.png")}/>
+                {
+                    selectedMosque==m.id?
+                  <Image className="w-[35px] h-[42px] scale-110 -translate-y-1"  source={require("./assets/markerActive.png")}/>:
+                  <Image className="w-[35px] h-[42px]"  source={require("./assets/marker.png")}/>
+                }
                 <Callout className="h-fit w-[200px] rounded-xl">
                     <Text className="text-lg">{m.name}</Text>
                     <Text className="py-1 text-xs">{m.name}</Text>
@@ -183,21 +275,28 @@ const mosques = [
           })
         }
 
+        <Marker
+          coordinate={mapRegion}
+          title="Your location"
+          className="z-10"
+        >
+          <Image className="w-[35px] h-[42px] " source={require("./assets/UserMarker.png")}/>
+        </Marker>
 
 
       </MapView>
-      <StatusBar style="light" />
+      <StatusBar style="dark" />
       {
         !openSearch &&
       <View className="absolute w-full bottom-0 p-2">
-        <View className="bg-[#e9f1ce] border border-white flex-row justify-between p-3  rounded-2xl  ">
+        <View className="bg-[#606c38]  flex-row justify-between p-3  rounded-2xl  ">
           <View>
-            <Text className="text-3xl text-[#606C38] font-semibold">03:15</Text>
-            <Text className="text-lg text-[#606C38] font-semibold">00:15:20</Text>
+            <Text className="text-3xl text-[#e9f1ce] font-semibold">03:15</Text>
+            <Text className="text-lg text-[#e9f1ce] font-semibold">00:15:20</Text>
           </View>
           <View>
-            <Text className="text-3xl text-[#606C38] font-semibold">صلاة العصر</Text>
-            <Text className="text-lg text-[#606C38] font-semibold">باق على الاذان</Text>
+            <Text className="text-3xl text-[#e9f1ce] font-semibold">صلاة العصر</Text>
+            <Text className="text-lg text-[#e9f1ce] font-semibold">باق على الاذان</Text>
           </View>
         </View>
       </View>
@@ -206,21 +305,42 @@ const mosques = [
 
       {
         openSearch &&
-        <View className="  absolute bottom-0 w-full">
+
+        <View  style={{ elevation:4}} className="  absolute bottom-0 w-full pt-1 bg-white border-t border-[#3333]">
+          <View className="flex-row justify-between px-2 items-center">
+
+          <Text className="py-2 text-2xl font-semibold text-[#606c38]">{
+          filteredMosquesState.length>2?
+          filteredMosquesState.length +" مساجد":
+          filteredMosquesState.length==2?
+          "مسجدين":
+          filteredMosquesState.length==1?
+          "مسجد واحد":"لم يتم العثور على اي مسجد"
+
+          } </Text>
+                  {selectedMosque!=null &&
+              <View >
+                <TouchableOpacity className="p-3 flex-row-reverse items-center py-1 bg-[#606c3822] rounded-lg text-lg">
+                  <Text className="text-sm ml-2">
+                    {mosques.filter(m=>m.id==selectedMosque)[0]?.name}
+                  </Text>
+                  <Icon name="arrow-back"></Icon>
+                </TouchableOpacity>
+              </View>
+              }
+          </View>
+
         <ScrollView  horizontal className="w-full ">
           {
-            mosques.filter(m=>m.name.toLocaleLowerCase().includes(search.toLocaleLowerCase())).map((m,key)=>(
-              <TouchableOpacity onPress={()=>GoTo(m.location)} >
-                <View key={key} style={{ elevation:2 }} className="w-fit t.shadow2xl  bg-[#eff1e6] m-2 rounded-lg overflow-hidden mx-1">
-                  <Image className="h-[100px] object-contain w-[200px] " source={{uri:m.img}}/>
-                  <View className=" absolute top-[65px] left-1 bg-[#0004] rounded-md">
+            filteredMosquesState.map((m,key)=>(
+              <TouchableOpacity key={key} onPress={()=>{GoTo(m.location);setSelectedMosque(m.id)}} >
+                <View  style={{ elevation:m.id==selectedMosque?6:3,top:m.id==selectedMosque?-6:0}} className="w-fit t.shadow2xl border-red-800 relative bg-[#eff1e6] m-2 rounded-lg overflow-hidden mx-1">
+                  <Image className="h-[80px] object-contain w-[130px] " source={{uri:m.img}}/>
+                  <View className=" absolute top-[45px] left-1 bg-[#0006] rounded-md">
                     <Text numberOfLines={1} className="px-1 text-white max-w-[140px] ">{m.name}</Text>
                     <Text numberOfLines={1} className="px-1 text-white max-w-[140px] text-xs  ">{m.name}</Text>
                   </View>
                   {/* <View className="flex-row p-1 justify-end">
-                    <TouchableOpacity className="  mr-1 p-1 px-3 w-fit justify-center items-center rounded-full bg-[#545c3999] ">
-                      <Text className="text-white">Show Road</Text>
-                    </TouchableOpacity>
                     <TouchableOpacity className="  mr-1 p-1 px-3 w-fit justify-center items-center rounded-full bg-[#545c3999] ">
                       <Text className="text-white">Open</Text>
                     </TouchableOpacity>
